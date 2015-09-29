622237cc088a5e3430c5982aa836f0c2789f555c
maradroid <jamaradroid@gmail.com>
2015-09-29 19:36:52 +0200

    version 1.0 (reworked info)

--- app/src/main/java/com/maradroid/shp/OAplikcijiActivity.java
+++ app/src/main/java/com/maradroid/shp/OAplikcijiActivity.java
@@ -1,10 +1,13 @@
 package com.maradroid.shp;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by mara on 9/24/15.
@@ -28,6 +31,39 @@ public class OAplikcijiActivity extends ActionBarActivity {
         toolbar.setTitleTextColor(Color.WHITE);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationIcon(R.mipmap.ic_chevron_left_white_36dp);
+    }
+
+    public void Link(View v){
+
+        int id = v.getId();
+
+        if(id == R.id.etfos) {
+
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.etfos.unios.hr/"));
+            startActivity(i);
+
+        }else if(id == R.id.ffos) {
+
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://web.ffos.hr/"));
+            startActivity(i);
+
+        }else if(id == R.id.vera) {
+
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ffos.unios.hr/hrvatski/vera-blazevic-krezic-znanstvena-novakinja"));
+            startActivity(i);
+
+        }else if(id == R.id.milica) {
+
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ffos.unios.hr/hrvatski/izv-prof-dr-sc-milica-lukic"));
+            startActivity(i);
+
+        }else if(id == R.id.balen) {
+
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.etfos.unios.hr/fakultet/imenik-djelatnika/jbalen"));
+            startActivity(i);
+
+        }
+
     }
 
     @Override
